181f4cbda93549c990b5dbab31c76151653eb9a4
xn00rz <36908103+xn00rz@users.noreply.github.com>
2018-03-02 23:46:22 -0300

    Create banner.py

---
+++ modules/banner.py
@@ -0,0 +1,39 @@
+from modules import colors
+
+class Banner():
+
+    def ennvy():
+
+        print(r'''
+        
+
+     (_(
+    ('')
+  _  "\ )>,_     .-->              
+  _>--w/((_ >,_.'
+         ///
+         "`"
+                      .-.
+         cebrutius!! /aa \_
+                   __\-  / )                 .-.
+         .-.      (__/    /  script kid !! _/oo \
+       _/ ..\       /     \               ( \v  /__
+      ( \  u/__    /       \__             \/   ___)
+       \    \__)   \_.-._._   )  .-.       /     \
+       /     \             `-`  / ee\_    /       \_
+    __/       \               __\  o/ )   \_.-.__   )
+   (   _._.-._/  Shit Tool!  (___   \/           '-'
+    '-'                        /     \
+                             _/       \    who is buru ?
+                            (   __.-._/
+         
+         
+         
+              by: xN00rZ¬¬
+
+        Grabber, a tool for grabber banners from a sub ip domain, and search vulnerabilities from them :p
+        
+        ''' + colors.Cores.pink)
+
+
+
